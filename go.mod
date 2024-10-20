5218ab2bc94002db968dcc20141e6aff1b0bca96
sovamorco <sovamorco@sovamor.co>
2024-10-20 18:01:26 +0400

    add log package

--- go.mod
+++ go.mod
@@ -2,8 +2,17 @@ module github.com/sovamorco/gommon
 
 go 1.22
 
+replace github.com/joomcode/errorx v1.1.1 => github.com/sovamorco/errorx v0.0.0-20240229170421-ba018a384e64
+
 require (
 	github.com/joomcode/errorx v1.1.1
 	github.com/mitchellh/mapstructure v1.5.0
+	github.com/rs/zerolog v1.33.0
 	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
---
+++ log/log.go
@@ -0,0 +1,68 @@
+package log
+
+import (
+	"io"
+	"os"
+
+	"github.com/joomcode/errorx"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type zerologWriter struct {
+	io.Writer
+	errWriter io.Writer
+}
+
+func (z *zerologWriter) WriteLevel(level zerolog.Level, p []byte) (int, error) {
+	writer := z.Writer
+	if level == zerolog.ErrorLevel {
+		writer = z.errWriter
+	}
+
+	n, err := writer.Write(p)
+	if err != nil {
+		return 0, errorx.Decorate(err, "write")
+	}
+
+	return n, nil
+}
+
+func InitLogger() zerolog.Logger {
+	//nolint:reassign // that's the way of zerolog.
+	zerolog.ErrorStackMarshaler = marshalErrorxStack
+
+	writer := zerologWriter{
+		Writer:    os.Stdout,
+		errWriter: os.Stderr,
+	}
+
+	logger := zerolog.New(writer).With().Caller().Timestamp().Stack().Logger().Level(zerolog.DebugLevel)
+
+	log.Logger = logger
+
+	return logger
+}
+
+func InitDevLogger() zerolog.Logger {
+	logger := InitLogger()
+
+	logger = logger.Level(zerolog.TraceLevel).Output(zerolog.NewConsoleWriter(
+		func(w *zerolog.ConsoleWriter) {
+			w.Out = os.Stderr
+		},
+	))
+	log.Logger = logger
+
+	return logger
+}
+
+//nolint:ireturn // that signature is required.
+func marshalErrorxStack(ierr error) any {
+	err := errorx.Cast(ierr)
+	if err == nil {
+		return nil
+	}
+
+	return err.MarshalStackTrace()
+}
