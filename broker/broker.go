a649131153ae578f2df1a44ba9a8a707c89c9431
sovamorco <sovamorco@sovamor.co>
2025-06-28 19:47:50 +0400

    add struct handler to broker

--- broker/broker.go
+++ broker/broker.go
@@ -2,6 +2,9 @@ package broker
 
 import (
 	"context"
+	"encoding/json"
+
+	"github.com/sovamorco/errorx"
 )
 
 type MessageHandler func(ctx context.Context, channel string, payload []byte) error
@@ -10,4 +13,19 @@ type Broker interface {
 	Subscribe(ctx context.Context, mh MessageHandler, channels ...string)
 	Publish(ctx context.Context, channel string, payload any) error
 	Shutdown(ctx context.Context)
+}
+
+type StructHandler[T any] func(ctx context.Context, channel string, payload T) error
+
+func StructToMessageHandler[T any](sh StructHandler[T]) MessageHandler {
+	return func(ctx context.Context, channel string, payload []byte) error {
+		var s T
+
+		err := json.Unmarshal(payload, s)
+		if err != nil {
+			return errorx.Wrap(err, "unmarshal payload")
+		}
+
+		return sh(ctx, channel, s)
+	}
 }
